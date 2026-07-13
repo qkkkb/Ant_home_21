7e201a90788258d6f73ae4d2db45c5100e7d3841
kkk <940714688@qq.com>
2026-07-13 18:56:11 +0800

    调整：提高从车速度环响应

--- New structure_2/models.py
+++ New structure_2/models.py
@@ -23,7 +23,7 @@ class SpeedPID:
         self.delta_ud = 0.0
         self.kd = 30.0
         self.gama = 0.25
-        self.kp = 1200.0
+        self.kp = 1800.0
         self.ki = 3.0
 
     def init_c(self):

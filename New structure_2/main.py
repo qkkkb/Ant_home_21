b39b7d645372d2f7aa5c734d808a148b2acfc5d2
kkk <940714688@qq.com>
2026-07-21 16:20:19 +0800

    修复：消除公转前馈冲突硬切换

--- New structure_2/main.py
+++ New structure_2/main.py
@@ -701,13 +701,13 @@ def solve_follow_pose_twist(
             target_ff_vy = ff_vy + ff_wz * Follow_Orbit_Target_Point_Wz_To_Vy
             target_ff_vx *= ff_scale
             target_ff_vy *= ff_scale
-            vx = add_feedforward_assist(
+            vx = add_feedforward_direct(
                 vx,
                 target_ff_vx,
                 Follow_Orbit_Feedforward_Forward_Gain,
                 Follow_Orbit_Feedforward_Forward_Limit,
             )
-            vy = add_feedforward_assist(
+            vy = add_feedforward_direct(
                 vy,
                 target_ff_vy,
                 Follow_Orbit_Feedforward_Lateral_Gain,

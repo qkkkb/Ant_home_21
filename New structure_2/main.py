51d7bdd1481f109c1377ea7381db382fcb37d74a
kkk <940714688@qq.com>
2026-07-22 16:54:26 +0800

    优化：减小普通跟随角度位置耦合

--- New structure_2/main.py
+++ New structure_2/main.py
@@ -677,7 +677,10 @@ def solve_follow_pose_twist(
         or error_angle <= -active_error
     )
     # Local pose features around the calibrated nonparallel formation.
-    cam_vx = error_x + error_angle
+    if orbit_mode or spin_mode:
+        cam_vx = error_x + error_angle
+    else:
+        cam_vx = error_x + error_angle * 0.5
     cam_vy = error_y
     if cam_vx > 0.0:
         cam_vy -= cam_vx * 5 // 13

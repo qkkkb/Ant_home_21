b21af15c757b9f8e9f3f27378a6ea3bb79b632fc
kkk <940714688@qq.com>
2026-07-28 22:06:33 +0800

    调整：轻量化降低推行视觉位置纠偏

--- New structure_2/main.py
+++ New structure_2/main.py
@@ -177,7 +177,6 @@ Follow_Orbit_Mode_FfWz_Off = 10.0
 Follow_Orbit_Mode_Exit_Ms = 60
 Follow_Orbit_Mode_FfWz_Filter = 0.22
 Follow_Normal_Wz_Feedforward_Limit = 15.0
-Follow_Spin_Mode_FfWz_On = 32.0
 Follow_Spin_Latch_Min_Wz = 26.0
 Follow_Spin_Command_Hold_Ms = 900
 Follow_Spin_Latch_Release_Angle = 3
@@ -619,8 +618,9 @@ def solve_follow_pose_twist(
     ff_vy,
     ff_wz,
     use_ff,
-    orbit_mode=False,
-    spin_mode=False,
+    orbit_mode,
+    spin_mode,
+    push_mode,
 ):
     vision_wz = calc_follow_angle(error_angle, orbit_mode, spin_mode)
     active_error = (
@@ -633,7 +633,7 @@ def solve_follow_pose_twist(
         or error_angle <= -active_error
     )
     # Local pose features around the calibrated nonparallel formation.
-    cam_vx = error_x + error_angle
+    cam_vx = error_x if push_mode else error_x + error_angle
     cam_vy = error_y
     if cam_vx > 0.0:
         cam_vy -= cam_vx * 5 // 13
@@ -648,6 +648,8 @@ def solve_follow_pose_twist(
     else:
         position_priority = False
     body_vx = -calc_follow_forward(cam_vx, position_priority)
+    if push_mode:
+        body_vx *= 0.55
     body_vy = calc_follow_lateral(cam_vy, position_priority)
     if (not orbit_mode) and (not spin_mode):
         body_vx *= Follow_Normal_Visual_Forward_Scale
@@ -1233,6 +1235,7 @@ def update_follow_targets(gyro_z):
             use_motion_feedforward,
             orbit_mode_active,
             spin_mode_active,
+            push_follow_active,
         )
         if follow_output_limit == FOLLOW_STATIC_LOCK_PWM_LIMIT:
             vx -= body_vx * (1.0 - Follow_Static_Visual_Scale)

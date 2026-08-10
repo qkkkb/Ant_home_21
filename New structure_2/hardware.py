3f54e3e96f9fcf816ee223803356ef6371dd671d
kkk <940714688@qq.com>
2026-08-10 20:39:41 +0800

    修复：增加从车电机非阻塞换向保护

--- New structure_2/hardware.py
+++ New structure_2/hardware.py
@@ -1,4 +1,8 @@
 from machine import Pin, PWM
+import utime
+
+
+_DIR_WAIT_MS = 20
 
 
 class Motor:
@@ -6,6 +10,8 @@ class Motor:
         self.invert = invert
         self.ph = Pin(ph_pin, Pin.OUT, value=0)
         self.pwm = PWM(pwm_pin, freq, duty_u16=0)
+        self.direction = 0
+        self.dir_wait_until = 0
 
     def duty(self, value, min_duty=0):
         value = int(value)
@@ -17,6 +23,23 @@ class Motor:
         dir_val = 1 if value > 0 else 0
         if self.invert:
             dir_val = 1 - dir_val
+        if dir_val != self.direction:
+            self.pwm.duty_u16(0)
+            self.ph.value(dir_val)
+            self.direction = dir_val
+            self.dir_wait_until = utime.ticks_add(
+                utime.ticks_ms(),
+                _DIR_WAIT_MS,
+            )
+            return 0
+        if self.dir_wait_until:
+            if utime.ticks_diff(
+                utime.ticks_ms(),
+                self.dir_wait_until,
+            ) < 0:
+                self.pwm.duty_u16(0)
+                return 0
+            self.dir_wait_until = 0
         self.ph.value(dir_val)
         self.pwm.duty_u16(abs(value))
         return value

d73cd7d56e0f9dc810c5ee7123eefe1acbf0a285
kkk <940714688@qq.com>
2026-08-16 22:40:40 +0800

    修复：统一推行阶段Y误差定义

--- New structure_2/main.py
+++ New structure_2/main.py
@@ -564,7 +564,6 @@ def solve_follow_pose_twist(
     cam_vy = error_y
     if push_mode:
         cam_vx += 6
-        cam_vy += 6
     if orbit_mode or spin_mode:
         position_priority = (
             (angle_active and (not spin_mode))
@@ -1564,7 +1563,7 @@ def update_follow_targets(gyro_z):
     elif push_follow_active:
         _pid_mod.push_correction_envelope(
             control_buf, follow_state, cam_error_x,
-            cam_error_y + (13 if explicit_push else 6),
+            cam_error_y,
             alloc_base_vx, explicit_push,
         )
         push_catchup_limit = control_buf[0]
@@ -1584,7 +1583,7 @@ def update_follow_targets(gyro_z):
         )
         if explicit_push and seen:
             body_vy = (
-                soft_deadband(cam_error_y + 13, 1, 2) * Follow_Lateral_Gain
+                soft_deadband(cam_error_y, 1, 2) * Follow_Lateral_Gain
                 - (actual_body_vy - alloc_base_vy)
                 * Follow_Push_Velocity_Damping
             )
